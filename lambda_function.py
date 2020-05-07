90f2f55ecaede44bfe267c3550a53a51b8bc2689
Paul Martins <paul.p.j.martins@gmail.com>
2020-05-07 13:27:16 +0100

    add non feasible check

--- lambda_function.py
+++ lambda_function.py
@@ -10,20 +10,116 @@ from collections import namedtuple
 from operator import attrgetter
 
 s3_client = boto3.client('s3')
-sqs_client = boto3.client('sqs')
 
 global SUDOKU_SIZE
 SUDOKU_SIZE = 9
 
-def numberIs(i, cube_solution):
-	if i > 0:
-		# return the matrix where number i is located
-		return cube_solution[i-1]
-	elif i == 0:
-		# return the matrix to say where the unknowed numbers are
-		return np.sum(cube_solution, axis=0)
+
+def lambda_handler(event, context):
+	# Read inputs
+	if 'Records' in event:
+		record = event['Records'][0] # only 1 record 
+		if 'eventSource' in record:
+			grid_id, input_matrix = sqs_handler(record)
+		elif 'eventName' in record:
+			grid_id, input_matrix = s3_handler(record)
+		else:
+			raise Exception(f'No eventName or eventSource found in record {record}')
 	else:
-		return f'Error: Pleased enter a number between 0 and {SUDOKU_SIZE}'
+		raise Exception(f'No Records found in event {event}')
+	print(f'Grid ID: {grid_id} \nInput matrix: \n {input_matrix}')
+	# Call solver
+	input_matrix_saved = [input_matrix.copy()]
+	solution, sol_found = solve_sudoku(grid_list = [input_matrix])
+	# Send solution to DynamoDB
+	dynamodb_table = boto3.resource('dynamodb').Table('sudokuGridRecords')
+	if sol_found:
+		print('Solution grid \n', solution)
+		send_sol_to_db = dynamodb_table.put_item(
+			Item = {
+			'grid_id': grid_id, 
+			'input': json.dumps(np.array(input_matrix_saved).ravel().tolist()),
+			'solution': json.dumps(np.array(solution).ravel().tolist()),
+			}
+		)
+	else:
+		print('No solution found')
+		send_sol_to_db = dynamodb_table.put_item(
+			Item = {
+			'grid_id': grid_id, 
+			'input': json.dumps(np.array(input_matrix_saved).ravel().tolist()),
+			'solution': 'No solution found',
+			}
+		)
+	if send_sol_to_db['ResponseMetadata']['HTTPStatusCode'] == 200:
+		print('Solution sent to Dynamodb')
+	else:
+		print(f'Error in writing solution for grid {grid_id} into DynamoDB')
+		raise Exception({send_sol_to_db["ResponseMetadata"]})
+
+
+def sqs_handler(record):
+	print(f'Reading event from {record["eventSource"]}')
+	grid_id = json.loads(record['body'])['grid_id']
+	input_numbers = json.loads(record['body'])['input_matrix']
+	SUDOKU_SIZE = int(np.sqrt(len(input_numbers)))
+	input_matrix = np.matrix(np.array(input_numbers).reshape((SUDOKU_SIZE, SUDOKU_SIZE)))
+	return grid_id, input_matrix
+
+
+def s3_handler(record):
+	# Download file from s3 bucket location to local
+	bucket = record['s3']['bucket']['name']
+	key = unquote_plus(record['s3']['object']['key'])
+	print(f'Reading event from {key}')
+	tmpkey = key.replace('/', '')
+	download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
+	s3_client.download_file(bucket, key, download_path)
+	grid_id = tmpkey.replace('outgoing', '').replace('.txt','')
+	# Open file
+	with open(download_path, 'r') as input_data_file:
+		input_data = input_data_file.read()
+	# Read the sudoku grid
+	lines = input_data.split('\n')
+	SUDOKU_SIZE = len(lines)
+	input_matrix = np.matrix([np.array(l.split(), dtype=int) for l in lines])
+	return grid_id, input_matrix
+
+
+def solve_sudoku(grid_list):
+	counter = 0
+	solution_found = False
+	while len(grid_list) > 0:
+		counter +=1
+		current_input_grid = grid_list.pop()
+		if 0 in current_input_grid:
+			cube_constraint, cube_solution = propagateConstraint(current_input_grid)
+			if gridIsNotFeasible(cube_solution):
+				break
+			grid_list += findNextGrids(cube_constraint, cube_solution, current_input_grid)
+			if counter % 1000 == 0:
+				print('Iterations:', counter, '| Queue length:', len(grid_list), '| Left to find:', 81-np.sum(numberIs(0,cube_solution),axis=None))
+		else:
+			solution_found = True
+			print(f'Solution found in {counter} iterations')
+			break
+	return current_input_grid, solution_found
+
+
+def gridIsNotFeasible(cube_solution):
+	feasibility = []
+	for number_grid in cube_solution:
+		# column check
+		col_feasibility = all(np.sum(number_grid, axis=0) < 2)
+		# row check
+		row_feasibility = all(np.sum(number_grid, axis=1) < 2)
+		# small square constraint
+		sqrt_size = int(np.sqrt(SUDOKU_SIZE))
+		number_grid_box = [np.sum(number_grid[int(sqrt_size*np.floor(i/sqrt_size)):int(sqrt_size*np.floor(i/sqrt_size)+sqrt_size), int(sqrt_size*i-SUDOKU_SIZE*np.floor(i/sqrt_size)):int(sqrt_size*i-SUDOKU_SIZE*np.floor(i/sqrt_size)+sqrt_size)], axis=None)< 2 for i in range(SUDOKU_SIZE)]
+		box_feasibility = all(number_grid_box)
+		feasibility += [col_feasibility and row_feasibility and box_feasibility]
+	return not all(feasibility)
+
 
 def propagateConstraint(current_input_matrix):
 	cube_solution = np.zeros((SUDOKU_SIZE, SUDOKU_SIZE, SUDOKU_SIZE)).astype(int)
@@ -46,9 +142,10 @@ def propagateConstraint(current_input_matrix):
 			row_e = int(sqrt_size*np.floor(i/sqrt_size)+sqrt_size)
 			col_s = int(sqrt_size*i-SUDOKU_SIZE*np.floor(i/sqrt_size))
 			col_e = int(sqrt_size*i-SUDOKU_SIZE*np.floor(i/sqrt_size)+sqrt_size)
-			if 1 in np.concatenate(mat[row_s:row_e, col_s:col_e]):
+			if 1 in mat[row_s:row_e, col_s:col_e]:
 				cube_constraint[idx][row_s:row_e, col_s:col_e] = np.ones((sqrt_size,sqrt_size))
 	return cube_constraint, cube_solution
+
 
 def findNextGrids(cube_constraint, cube_solution, current_input_matrix):
 	# check which numbers are fully constraint (8) and still unknown
@@ -87,117 +184,20 @@ def findNextGrids(cube_constraint, cube_solution, current_input_matrix):
 	return grid_output
 
 
-def solve_sudoku(grid_list):
-	counter = 0
-	solution_found = False
-	while len(grid_list) > 0:
-		counter +=1
-		current_input_grid = grid_list.pop()
-		if 0 in current_input_grid:
-			cube_constraint, cube_solution = propagateConstraint(current_input_grid)
-			grid_list += findNextGrids(cube_constraint, cube_solution, current_input_grid)
-			if counter % 1000 == 0:
-				print('Iterations:', counter, '| Queue length:', len(grid_list), '| Left to find:', 81-np.sum(numberIs(0,cube_solution),axis=None))
-		else:
-			solution_found = True
-			print(f'Solution found in {counter} iterations')
-			break
-	return current_input_grid, solution_found
-
-
-def sqs_handler(record):
-	print(f'Reading event from {record["eventSource"]}')
-	grid_id = json.loads(record['body'])['grid_id']
-	input_numbers = json.loads(record['body'])['input_matrix']
-	SUDOKU_SIZE = int(np.sqrt(len(input_numbers)))
-	input_matrix = np.matrix(np.array(input_numbers).reshape((SUDOKU_SIZE, SUDOKU_SIZE)))
-	return grid_id, input_matrix
-
-
-def s3_handler(record):
-	# Download file from s3 bucket location to local
-	bucket = record['s3']['bucket']['name']
-	key = unquote_plus(record['s3']['object']['key'])
-	print(f'Reading event from {key}')
-	tmpkey = key.replace('/', '')
-	download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
-	s3_client.download_file(bucket, key, download_path)
-	grid_id = tmpkey.replace('outgoing', '').replace('.txt','')
-	# Open file
-	with open(download_path, 'r') as input_data_file:
-		input_data = input_data_file.read()
-	# Read the sudoku grid
-	lines = input_data.split('\n')
-	SUDOKU_SIZE = len(lines)
-	input_matrix = np.matrix([np.array(l.split(), dtype=int) for l in lines])
-	return grid_id, input_matrix
-
-
-def lambda_handler(event, context):
-	print('Event received \n', event)
-	# AWS resources ID
-	all_queue_urls = sqs_client.list_queues()['QueueUrls']
-	dl_queue_url = [url for url in all_queue_urls if 'sudoku-grid-requested-dead-letter' in url][0]
-	dynamodb_table = boto3.resource('dynamodb').Table('sudokuGridRecords')
-	# Read inputs
-	if 'Records' in event:
-		for record in event['Records']:
-			if 'eventSource' in record:
-				grid_id, input_matrix = sqs_handler(record)
-			elif 'eventName' in record:
-				grid_id, input_matrix = s3_handler(record)
-		print(f'Grid ID: {grid_id}')
-		print(input_matrix)	
-		# Call solver
-		input_matrix_saved = [input_matrix.copy()]
-		solution, sol_found = solve_sudoku(grid_list = [input_matrix])
-		# Send solution to DynamoDB
-		if sol_found:
-			print('Solution grid \n', solution)
-			send_sol_to_db = dynamodb_table.put_item(
-				Item = {
-				'grid_id': grid_id, 
-				'input': json.dumps(np.array(input_matrix_saved).ravel().tolist()),
-				'solution': json.dumps(np.array(solution).ravel().tolist()),
-				}
-			)
-			if send_sol_to_db['ResponseMetadata']['HTTPStatusCode'] == 200:
-				print('Solution sent to Dynamodb')
-			else:
-				response = sqs.send_message(
-					QueueUrl=dl_queue_url,
-					DelaySeconds=5,
-					MessageAttributes={
-					'Title': {
-						'DataType': 'String',
-						'StringValue': 'Dynamo DB writing error'
-						},
-					'Author': {
-						'DataType': 'String',
-						'StringValue': 'AWS Lambda sudokuSolver'
-						},
-					},
-					MessageBody=(f'Error in writing solution found for grid {grid_id} into DynamoDB',
-						f'Response: {send_sol_to_db["ResponseMetadata"]}')
-					)
-				print(response['MessageId'])
-		else:
-			print('No solution found')
-			dynamodb_table = boto3.resource('dynamodb').Table('sudokuGridRecords')
-			send_sol_to_db = dynamodb_table.put_item(
-				Item = {
-				'grid_id': grid_id, 
-				'input': json.dumps(np.array(input_matrix_saved).ravel().tolist()),
-				'solution': 'No solution found',
-				}
-			)
-		print('Event processed')
+def numberIs(i, cube_solution):
+	if i > 0:
+		# return the matrix where number i is located
+		return cube_solution[i-1]
+	elif i == 0:
+		# return the matrix to say where the unknowed numbers are
+		return np.sum(cube_solution, axis=0)
 	else:
-		print('Cannot process event')
+		return f'Error: Pleased enter a number between 0 and {SUDOKU_SIZE}'
 
 	
 
 # Deprecated, used for direct invocation from API Gateway but browser timed out before the hardest grid were solved
+# Solved by using decoupled invocation
 #
 #	def api_handler(event):
 #		print(f'Reading event from API call')
